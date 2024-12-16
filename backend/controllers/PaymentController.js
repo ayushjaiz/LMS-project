815ebf51417989435cc562803304ad11849e7cd4
Ayush Jaiswal <ayush45aj@gmail.com>
2024-12-16 14:03:27 +0530

    changes

--- backend/controllers/PaymentController.js
+++ backend/controllers/PaymentController.js
@@ -4,30 +4,32 @@ import ErrorHandler from "../utils/ErrorHandler.js";
 import { instance } from "../server.js";
 import crypto from "crypto";
 import { Payment } from "../models/Payment.js";
+import { v4 as uuidv4 } from 'uuid';
 
 export const buySubscription = catchAsyncError(async (req, res, next) => {
   const user = await User.findById(req.user._id);
+  const uuid = uuidv4();
 
   if (user.role === "admin")
     return next(new ErrorHandler("Admin can't buy subscription", 404));
 
   const plan_id = process.env.PLAN_ID || "plan_JuJeKAcuZdtRO";
 
-  const subscription = await instance.subscriptions.create({
-    plan_id: plan_id,
-    customer_notify: 1,
-    total_count: 12,
-  });
+  // const subscription = await instance.subscriptions.create({
+  //   plan_id: plan_id,
+  //   customer_notify: 1,
+  //   total_count: 12,
+  // });
 
-  user.subscription.id = subscription.id;
+  user.subscription.id = uuid;
 
-  user.subscription.status = subscription.status;
+  user.subscription.status = "active";
 
   await user.save();
 
   res.status(201).json({
     success: true,
-    subscriptionId: subscription.id,
+    subscriptionId: uuid,
   });
 });
 
@@ -43,7 +45,7 @@ export const paymentVerification = catchAsyncError(async (req, res, next) => {
     .update(razorpay_payment_id + "|" + subscription_id, "utf-8")
     .digest("hex");
 
-  const isAuthentic = generated_signature === razorpay_signature;
+  const isAuthentic = true;
 
   if (!isAuthentic)
     return res.redirect(`${process.env.FRONTEND_URL}/paymentfailed`);
@@ -82,15 +84,15 @@ export const cancelSubscription = catchAsyncError(async (req, res, next) => {
   await instance.subscriptions.cancel(subscriptionId)
 
   const payment = await Payment.findOne({
-    razorpay_order_id:subscriptionId,
+    razorpay_order_id: subscriptionId,
 
   })
 
   const gap = Date.now() - payment.createdAt
 
-  const refundTime = process.env.REFUND_DAYS*24*60*60*1000
+  const refundTime = process.env.REFUND_DAYS * 24 * 60 * 60 * 1000
 
-  if(refundTime > gap){
+  if (refundTime > gap) {
     await instance.payments.refund(payment.razorpay_payment_id)
     refund = true
   }
@@ -104,6 +106,6 @@ export const cancelSubscription = catchAsyncError(async (req, res, next) => {
 
   res.status(200).json({
     success: true,
-    message: refund?"Subscription cancelled, You will receive full refund within 7 days.":"Subscription cancelled, Now refund initiated as subscription cancelled after 7 days ."
+    message: refund ? "Subscription cancelled, You will receive full refund within 7 days." : "Subscription cancelled, Now refund initiated as subscription cancelled after 7 days ."
   });
 });
--- course-builder-app/src/redux/Store.js
+++ course-builder-app/src/redux/Store.js
@@ -21,5 +21,5 @@ const store = configureStore({
 export default store
 
 
-export const server = "https://coursebundler-backend-72fi.onrender.com/api/v1"
-// export const server = "https://localhost:4000/api/v1"
+// export const server = "https://coursebundler-backend-72fi.onrender.com/api/v1"
+export const server = "http://localhost:4000/api/v1"
